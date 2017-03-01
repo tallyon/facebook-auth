530e990b0bf3ef6cba07797a1b6d55f482e61c79
Tallyon <skowronski6@gmail.com>
2017-03-01 09:21:04 +0100

    facebook auth using native nodejs http module

--- README.md
+++ README.md
@@ -1,26 +1,20 @@
+## Endpoints
+
+Server runs two endpoints: GET /auth and GET /callback.
+GET /auth should be called to redirect to facebook auth dialog.
+GET /callback is used only when redirectUrl command line argument is not provided and it will receive callback with token from facebook auth dialog.
+
 ## Usage
 
 ```bash
-node server.js --sendTokenUrl=localhost:3001/token --redirectUrl=google.pl:80/
+node server.js --redirectUrl=localhost:3000/callback
 ```
 This will start facebook authorization server that will listen on localhost:3000/auth and redirect to facebook auth dialog.
-Upon successful authorization server will POST the json token to 'sendTokenUrl' with body:
+Upon successful authorization Facebook auth dialog will callback to url provided in redirectUrl with token in url query like this:
 
-```json
-{
-    "token": "EADDADADFAEEG3252525vv53v5V3432vRFVABM34"
-}
 ```
-
-User will be then redirected to 'redirectUrl'.
-
-To test you can run this server with command provided in Usage and run token receiving server with
-
-```bash
-node index.js
+    http://localhost:3000/callback?code=AQC10vNLJVtvAT5pk1rbz1zvx5yqd7YzxaE52XVpe8Y2j6QElF3WyaIcvGTZoc-MkS6agQLxvJLEipJVTiwa29N6Q8k0-yERh1yzitT1zJ93wnWpP0NRqTYElN6Y4eyJnoosu8xkNiI4C8k4kXj3vCSdQ09BBS5m2UbqjKnKAlBlvT7fVEDn-viMb2Y5MTliTRhzMgizaN64EZZdARD1v85FygLt8VttUWqP01lsr5DNuyZTzfgjcnTv1LFoT-Mt7nAEOx0JLymSTAfrhyHWRkqDQLhKpU4p4WfRX3ZSG9CZk5KRBR71Y4J#_=_
 ```
-
-which will start second server on port 3001 that will be listening on port 3001 for POST /token and will print received token in the console.
 
 ## Config
 Config file can be found in /config/default.json
--- server.js
+++ server.js
@@ -1,90 +1,104 @@
-var express = require("express");
-var passport = require("passport");
-var Strategy = require("passport-facebook").Strategy;
 var http = require("http");
 var config = require("config");
 var argv = require("yargs").argv;
+var request = require("request");
+var simpleOauth2 = require("simple-oauth2");
+var url = require("url");
 
 // Get config from /config/default.json
 const port = config.get("App").port;
+// If port was not provided in config assume port 3117
+if(port == null) {
+    port = 3117;
+    console.log("Port was not provided in config file! Using default: " + port);
+}
+
 const facebookClientID = config.get("Facebook").facebookAPPClientID;
+if(facebookClientID == null || facebookClientID.length < 1) {
+    console.error("No facebook client ID provided in config file!");
+    process.exit(1);
+}
 const facebookSecret = config.get("Facebook").facebookAPPSecret;
+if(facebookSecret == null || facebookSecret.length < 1) {
+    console.error("No facebook secret provided in config file!");
+    process.exit(1);
+}
 const facebookCallbackUrl = config.get("Facebook").facebookCallbackUrl;
 
-// sendTokenUrl should be url to call and post token data: localhost:3001/token
-var tokenPath = SplitPath(argv.sendTokenUrl);
-
 // redirectUrl should be redirect url to provide to passport
-var redirectPath = SplitPath(argv.redirectUrl);
+var redirectPath = null;
+if(argv.redirectUrl != null) {
+    redirectPath = SplitPath(argv.redirectUrl);
+}
+// If command line argument redirectUrl was not provided assume that callback should be to this server on GET /callback
+else {
+    redirectPath = SplitPath("localhost:" + port + "/callback");
+    console.log("redirectUrl argument was not provided! Assuming " + redirectPath.fullPath + " as callback url");
+}
 
-// Configure passport-facebook
-passport.use(new Strategy(
-    {
-        clientID: facebookClientID,
-        clientSecret: facebookSecret,
-        callbackURL: facebookCallbackUrl
-    }, function(accessToken, refreshToken, profile, cb) {
-        OnSuccess(accessToken, profile);
-        console.log("access token:", accessToken, "profile", profile);
-        return cb(null, profile);
+var oauth2 = simpleOauth2.create({
+    // Facebook app details
+    client: {
+        id: facebookClientID,
+        secret: facebookSecret
+    },
+    // Facebook token uri
+    auth: {
+        tokenHost: "https://www.facebook.com",
+        tokenPath: "/v2.8/dialog/oauth",
+        authorizePath: "/v2.8/dialog/oauth"
     }
-    ));
-
-var server = express();
-
-server.use(require("body-parser").urlencoded({ extended: true }));
-server.use(passport.initialize());
-
-// GET /auth will start authentication and show facebook dialog
-server.get("/auth", passport.authenticate("facebook", { session: false }));
-
-// GET /redirect will check if user is authenticated and either redirect to GET /success or GET /err
-server.get("/redirect",
-    passport.authenticate("facebook", { session: false, successRedirect: "/success", failureRedirect: "/err" }));
-
-server.get("/err", function(req, res) {
-    res.status(401).json({message:"ERROR"}).end();
 });
 
-// GET /success will redirect to path provided in command line argument redirectUrl
-server.get("/success", function(req, res) {
-    res.redirect(redirectPath.fullPath);
+// Authorization uri definition
+const authorizationUri = oauth2.authorizationCode.authorizeURL({
+    redirect_uri: redirectPath.fullPath,
+    scope: "",
+    state: ""
 });
 
-server.listen(3000, function() {
+var server = http.createServer((req, res) => {
+    var parsedUrl = url.parse(req.url, true);
+
+    if(parsedUrl.pathname == "/auth") {
+        req.setEncoding("utf8");
+
+        // Redirect to Facebook auth dialog
+        res.writeHead(301, { Location: authorizationUri });
+        res.end();
+    }
+    else if(parsedUrl.pathname == "/callback") {
+        res.writeHead(200, {"Content-Type": "text/plain"});
+
+        // Returned token should be "code" in url query
+        var authToken = {
+            token: parsedUrl.query.code
+        };
+
+        if(authToken.token != null) {
+            res.write(authToken.token);
+        }
+        else {
+            res.write("error: no token received");
+        }
+        res.end();
+
+        // After getting token redirect to provided uri
+    }
+    else {
+        // Handle errors?
+        // path mismatch, access restriction?
+        // 404?
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end("not found");
+    }
+});
+
+// Start the server
+server.listen(port, () => {
     console.log("Listening on port", port);
-    console.log("Token will be sent via POST to", tokenPath.fullPath);
     console.log("User will be redirected to", redirectPath.fullPath);
 });
-
-function OnError() {
-    err = new Error("failed to authenticate");
-    console.log(err);
-}
-
-// Sends authentication token via POST method to url provided in command line argument sendTokenUrl
-function OnSuccess(token, profile) {
-    var sendSuccess = {
-        "token": token,
-        // "timestamp": Date.now(),
-        // "profile": profile
-    };
-
-    var r = http.request({
-        host: tokenPath.host,
-        method: "POST",
-        port: tokenPath.port,
-        path: tokenPath.path,
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }, function(res) {
-
-    });
-    r.write(JSON.stringify(sendSuccess));
-    console.log("sending token via POST to", tokenPath.fullPath);
-    r.end();
-}
 
 // Takes string "http://hostname:8080/path/to/resource" and converts it to object with fields:
 //  {
